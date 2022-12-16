8823af235d0ab0051f057a36b73b911ca1aaf09d
the-horseman <aryanrishi20@gmail.com>
2022-12-16 16:38:48 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import Search_bar from './Components/Search_bar';
-import Movie_grid from './Components/Movie_grid';
+import SearchBar from './Components/SearchBar';
+import MovieGrid from './Components/MovieGrid';
 import logo from "./logo.svg";
 
 function App() {
@@ -14,7 +14,10 @@ function App() {
     <div className='app'>
       <div className='nav-bar'>
         <div className='company-logo'>
-          <img src={logo} onClick={() => {
+          <img 
+          src={logo}
+          alt={"company logo"} 
+          onClick={() => {
             axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
               .then((response) => {
                 setMovies(response.data.results)
@@ -23,11 +26,11 @@ function App() {
             style={{ cursor: 'pointer' }}
           ></img>
         </div>
-        <Search_bar setMovies={setMovies} />
+        <SearchBar setMovies={setMovies} />
       </div>
       <div className='line'></div>
       <div className='recent-grid'>
-        <Movie_grid movies={movies} setMovies={setMovies} />
+        <MovieGrid movies={movies} setMovies={setMovies} />
       </div>
     </div>
   );
